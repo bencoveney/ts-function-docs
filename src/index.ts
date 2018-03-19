7c7aa86d24ca85eb897c08125c127782ca4e104c
Martin Stephen Cox <admin@martinstephencox.com>
2018-03-19 20:46:34 +0000

    Added input and output args

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import * as yargs from "yargs";
+
 import { getFiles } from "./getFiles";
 import { buildModel } from "./buildModel";
 import { createDocumentation } from "./createDocumentation";
 import { Class } from "./model";
+import { Argv } from "yargs"
 
-getFiles("input/**/*.ts")
+const args = yargs.usage("Usage: $0 --input [string] --output [string]")
+    .option("input", { description: "The input directory", alias: "i", default: "input/**/*.ts" })
+    .option("output", { description: "The output directory", alias: "o", default: "output/documentation.md" })
+    .argv;
+
+getFiles(args.input)
     .then(buildModel)
-    .then(classes => createDocumentation("output/documentation.md", classes))
+    .then(classes => createDocumentation(args.output, classes))
     .catch(console.error);
